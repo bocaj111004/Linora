a572afca63a6964458d4f00eeab4bd4fec67fb84
bocaj111004 <bocaj111004@gmail.com>
2025-02-27 17:57:09 +0000

    Update ThemeManager.lua 3

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -172,11 +172,11 @@ local ThemeManager = {} do
 		groupbox:AddDropdown('ThemeManager_ThemeList', { Text = 'Theme list', Values = ThemesArray, Default = 1 })
 		groupbox:AddButton('Set as default', function()
 			self:SaveDefault(getgenv().Linoria.Options.ThemeManager_ThemeList.Value)
-			self.Library:Notify(string.format('Set default theme to %q', getgenv().Linoria.Options.ThemeManager_ThemeList.Value))
+			self.Library:Notify(string.format('Set default theme to %q', Options.ThemeManager_ThemeList.Value))
 		end)
 
 		getgenv().Linoria.Options.ThemeManager_ThemeList:OnChanged(function()
-			self:ApplyTheme(getgenv().Linoria.Options.ThemeManager_ThemeList.Value)
+			self:ApplyTheme(Options.ThemeManager_ThemeList.Value)
 		end)
 
 		groupbox:AddDivider()
